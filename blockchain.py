3decf97d324e7cca00e465271878d2e560d23b5a
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-01-16 14:06:28 +0900

    Update new_transactions

--- blockchain.py
+++ blockchain.py
@@ -7,8 +7,24 @@ class Blockchain(object):
     def new_block(self):
         pass
 
-    def new_transactions(self):
-        pass
+    def new_transactions(self, sender, recipient, amount):
+
+        """
+        :param sender: address of sender
+        :param recipient: address for receive
+        :param amount: amount of cryptocurrency
+        :return: next block of last block
+
+        method for make new transactions
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(self):
@@ -18,4 +34,3 @@ class Blockchain(object):
     def last_block(self):
         pass
 
-    
