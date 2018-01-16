8b3762e652b5dc6ed44f683efef9cf78e9441d10
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-01-16 13:59:27 +0900

    Update blockchain structure

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transactions(self):
+        pass
+
+    @staticmethod
+    def hash(self):
+        pass
+
+    @property
+    def last_block(self):
+        pass
+
+    
