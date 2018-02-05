e4b3c62ffcbb766127a694f451369aa37153725a
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-02-05 20:20:25 +0900

    make resolve consensus method

--- models/Block.py
+++ models/Block.py
@@ -1,9 +1,8 @@
-from models.Transactions import *
 from mongoengine import *
 import datetime
 
+connect('PyCoin')
 
-connect('BlockChain')
 
 class Block(Document):
 
@@ -26,3 +25,18 @@ class Block(Document):
     previous_hash = StringField(
         required=True
     )
+
+    @staticmethod
+    def resolve_consensus(new_chain):
+
+        Block.drop_collection()
+
+        for block in new_chain:
+            Block(index=block["index"],
+                  timestamp=block["timestamp"],
+                  transactions=block['transactions'],
+                  proof=block["proof"],
+                  previous_hash=block["previous_hash"]
+                  ).save()
+
+            return True
