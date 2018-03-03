138088548ede2651643be46cd2bcbdb4cae377ba
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-03-03 10:00:43 +0900

    exceptions

---
+++ PyCoin/exceptions.py
@@ -0,0 +1,19 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class DBAccessError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+class NodeUnavailableError(Error):
+
+    def __init__(self, msg="Node unavailable"):
+        self.msg = msg
