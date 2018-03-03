4a9bf34f297794efa7a9071a7c7fe46d821cc37c
Lewis Kim (seonghyeon Kim) <kshr2d2@gmail.com>
2018-03-03 10:00:58 +0900

    new hashcash

--- PyCoin/hash_cash.py
+++ PyCoin/hash_cash.py
@@ -1,36 +1,36 @@
+from .. import CONFIG
 import hashlib
 import json
 
 
-class HashCash:
+def block_hash(block):
+    block_string = json.dumps(block, sort_keys=True)
+    return hashlib.sha256(str(block_string).encode()).hexdigest()
 
-    @staticmethod
-    def hash(block):
-        block_string = json.dumps(block, sort_keys=True)
-        return hashlib.sha256(str(block_string).encode()).hexdigest()
 
-    @staticmethod
-    def proof_of_work(last_proof):
-        """
-        :param last_proof: last proof
-        :return: matched proof
-        """
+def proof_of_work(last_proof):
+    """
+    :param last_proof: String
+    :return: Int
+    """
 
-        proof = 0
+    proof = 0
 
-        while HashCash.valid_proof(last_proof, proof) is False:
-            proof += 1
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
-        return proof
+    return proof
 
-    @staticmethod
-    def valid_proof(last_proof, proof):
-        """
-        :param last_proof:
-        :param proof:
-        :return:
-        """
 
-        guess = str(last_proof * proof).encode()
-        return hashlib.sha256(guess).hexdigest()[:6] == '000000'  # set difficulty
+def valid_proof(last_proof, proof):
+    """
+    :param last_proof: String
+    :param proof: Int
+    :return: Boolean
+    """
+
+    guess = str(last_proof * proof).encode()
+
+    return hashlib.sha256(guess).hexdigest()[:6] == '0' * CONFIG.DIFFICULTY  # set difficulty
+
 
